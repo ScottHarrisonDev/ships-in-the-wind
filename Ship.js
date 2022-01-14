dfd46ce08e7ee6abb7d5c895b6801109d9277031
Scott Harrison <github@scottharrisondesign.co.uk>
2022-01-14 19:03:32 +0000

    ship now follows mouse

--- Ship.js
+++ Ship.js
@@ -4,9 +4,8 @@ class Ship {
         this.width = 32
         this.height = 32
         this.direction = 'right'
-        this.moving = false
         this.reloading = false
-        this.maxSpeed = 2
+        this.maxSpeed = 1
         this.pos = createVector(x, y)
         this.image = preloadedData.spritesheet
         this.imageData = preloadedData.spritedata.frames
@@ -16,51 +15,37 @@ class Ship {
         return frames.find(frame => frame.name === `ship-${direction}`)
     }
 
-    isMoving() {
-        if (
-            keyIsDown(UP_ARROW) ||
-            keyIsDown(DOWN_ARROW) ||
-            keyIsDown(LEFT_ARROW) ||
-            keyIsDown(RIGHT_ARROW)
-        ) {
-            return true
-        }
-        return false
-    }
+    getDirection(currentPos, speed) {
+        const velocityVector = this.getVelocityVector(currentPos, speed)
+        const currentAngle = degrees(velocityVector.heading()).toFixed(2)
 
-    getDirection() {
-        if (keyIsDown(UP_ARROW)) {
-            return 'up'
-        }
-        if (keyIsDown(DOWN_ARROW)) {
-            return 'down'
-        }
-        if (keyIsDown(LEFT_ARROW)) {
-            return 'left'
-        }
-        if (keyIsDown(RIGHT_ARROW)) {
-            return 'right'
+        switch (true) {
+            case (currentAngle > -135 && currentAngle < -45):
+                return 'up';
+            case (currentAngle > 45 && currentAngle < 135):
+                return 'down';
+            case (currentAngle > 135 || currentAngle < -135):
+                return 'left';
+            case (currentAngle > -45 && currentAngle < 45):
+                return 'right';
         }
     }
 
-    getNewPosition(currentPos, direction, speed) {
-        let movement
-        switch (direction) {
-            case 'up':
-                movement = createVector(0, -speed)
-                break
-            case 'down':
-                movement = createVector(0, speed)
-                break
-            case 'left':
-                movement = createVector(-speed, 0)
-                break
-            case 'right':
-                movement = createVector(speed, 0)
-                break
-        }
+    getVelocityVector(currentPos, speed) {
+        const mousePos = createVector(mouseX, mouseY)
+        const distance = mousePos.dist(currentPos)
+        const distanceVector = mousePos.sub(currentPos)
+        const normalizedPos = distanceVector.normalize()
+        const mappedDistance = map(distance, 100, 0, speed, 0.5)
+        const newPosVector = normalizedPos.mult(mappedDistance)
 
-        return currentPos.add(movement)
+        return newPosVector
+    }
+
+    getNewPosition(currentPos, speed) {
+        const velocityVector = this.getVelocityVector(currentPos, speed)
+
+        return currentPos.add(velocityVector)
     }
 
     draw() {
@@ -79,11 +64,8 @@ class Ship {
     }
 
     update() {
-        this.moving = this.isMoving()
-        if (this.moving) {
-            this.direction = this.getDirection()
-            this.pos = this.getNewPosition(this.pos, this.direction, this.maxSpeed)
-        }
+        this.pos = this.getNewPosition(this.pos, this.maxSpeed)
+        this.direction = this.getDirection(this.pos, this.maxSpeed) || this.direction
         this.draw()
     }
 
